67f858f58870d6ae7e84a8de72e0b3e038a5ff2d
SuzanaMorais7 <suzanamorais02@gmail.com>
2026-03-21 22:06:59 -0300

    Add files via upload

---
+++ ex6.rs
@@ -0,0 +1,9 @@
+def potencias_de_dois(n): 
+    i = 1 
+while i < n: 
+        print(i) 
+        i *= 
+2
+// Complexidade: O(log n)
+// Começa com o número 1 e vai multiplicando por 2 até chegar perto do número informado.
+// O valor dobra a cada passo, então não precisa de muitas repetições. Por isso cresce devagar.
